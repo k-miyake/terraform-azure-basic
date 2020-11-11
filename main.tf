938208a7d2359d7b207f96d12df6ce3148990cb3
Kazuyuki Miyake <k-miyake@zengeeks.io>
2020-11-11 18:53:33 +0900

    add function app

--- main.tf
+++ main.tf
@@ -18,5 +18,35 @@ provider "azurerm" {
 // リソースグループ
 resource "azurerm_resource_group" "default" {
   name     = "rg-tfazure-demo"
-  location = "japaneast"
+  location = "westus2"
+}
+
+// ストレージアカウント
+resource "azurerm_storage_account" "default" {
+  name                     = "sttfazuredemo"
+  resource_group_name      = azurerm_resource_group.default.name
+  location                 = azurerm_resource_group.default.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "default" {
+  name                = "plan-tfazure-demo"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  kind                = "FunctionApp"
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "default" {
+  name                       = "func-tfazure-demo"
+  location                   = azurerm_resource_group.default.location
+  resource_group_name        = azurerm_resource_group.default.name
+  app_service_plan_id        = azurerm_app_service_plan.default.id
+  storage_account_name       = azurerm_storage_account.default.name
+  storage_account_access_key = azurerm_storage_account.default.primary_access_key
 }
